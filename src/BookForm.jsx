1684ac357a8a9494aca543929e1b5022392f2cdc
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-06-30 19:55:47 -0400

    commit
    styling modal

--- src/BookForm.jsx
+++ src/BookForm.jsx
@@ -9,53 +9,55 @@ const BookForm = () => {
     return (
     <div className="overlay">
         <div className="modalContainer">
-        <Button color="danger" onClick={toggle}>
-            Recommend A Book    
-        </Button>
+            <Button color="danger" onClick={toggle}>
+                Recommend A Book    
+            </Button>
 
-        <Modal isOpen={formDisplay}>
-            <ModalHeader>            
-                <h2>Recommend A Book</h2>
-            </ModalHeader>
-            <ModalBody>
-                <Formik>
-                        <Form>
-                            <Field
-                                name="title"
-                                type="text"
-                                placeholder="Title"
-                            >
-                            </Field>
-                            <Field
-                                name="author"
-                                type="text"
-                                placeholder="Author"
-                            >
-                            </Field>
-                            <Field
-                                label="Genres"
-                                name="title"
-                                type="select"
-                                placeholder="Select Genres"
-                            >
-                                <option value="romance"></option>
-                                <option value="hero-quest"></option>
-                                <option value="sci-fi"></option>
-                                <option value="fantasy"></option>
-                                <option value="mystery"></option>
-                                <option value="biography"></option>
-                                <option value="historical-fiction"></option>
-                            </Field>
+            <Modal isOpen={formDisplay}>
+                <ModalHeader>            
+                    <h2>Recommend A Book</h2>
+                </ModalHeader>
+                <ModalBody>
+                    <Formik>
+                            <Form>
+                                <Field
+                                    name="title"
+                                    type="text"
+                                    placeholder="Title"
+                                >
+                                </Field>
+                                <Field
+                                    name="author"
+                                    type="text"
+                                    placeholder="Author"
+                                >
+                                </Field>
+                                <Field
+                                    label="Genres"
+                                    name="title"
+                                    type="select"
+                                    placeholder="Select Genres"
+                                >
+                                    <option value="romance"></option>
+                                    <option value="hero-quest"></option>
+                                    <option value="sci-fi"></option>
+                                    <option value="fantasy"></option>
+                                    <option value="mystery"></option>
+                                    <option value="biography"></option>
+                                    <option value="historical-fiction"></option>
+                                </Field>
 
-                        </Form>
-                    </Formik>
-            </ModalBody>
-            <ModalFooter>
-                <Button type="submit">Submit</Button>
-                <Button onClick={toggle}>Close</Button>
-            </ModalFooter>
-        </Modal>
-    </div>
+                            </Form>
+                        </Formik>
+                </ModalBody>
+                <ModalFooter>
+                    <div className="btn-container">
+                    <Button type="submit">Submit</Button>
+                    <Button onClick={toggle}>Close</Button>
+                    </div>
+                </ModalFooter>
+            </Modal>
+        </div>
     </div>
     )
 }
