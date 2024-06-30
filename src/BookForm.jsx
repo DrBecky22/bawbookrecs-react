8cb27df091779e0104ceff6e4f26bca434018cb2
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-06-30 19:09:11 -0400

    commit
    working on modal overlay - duplicate code will need to be thinned

--- src/BookForm.jsx
+++ src/BookForm.jsx
@@ -6,6 +6,42 @@ const BookForm = () => {
     const [formDisplay, setFormDisplay] = useState(false);
     const toggle = () => setFormDisplay(!formDisplay);
 
+    <div className="overlay">
+        <div className="modalContainer">
+            <div className="modalHeader">
+                <h2>Recommend A Book</h2>
+            </div>
+            <div className="modalBody">
+                <Formik>
+                    <Form>
+                        <Field
+                            name="title"
+                            type="text"
+                            placeholder="Title"
+                        >
+                        </Field>
+                        <Field
+                            name="author"
+                            type="text"
+                            placeholder="Author"
+                        >
+                        </Field>
+                        <Field
+                            label="Genres"
+                            name="title"
+                            type="text"
+                            placeholder="Select Genres"
+                        >
+                        </Field>
+
+                    </Form>
+                </Formik>
+            </div>
+            <div className="modalFooter">
+                <Button type="submit">Submit</Button>
+                <Button onClick={toggle}>Close</Button>
+        </div>
+    </div>
     return (
         <div>
         <Button color="danger" onClick={toggle}>
